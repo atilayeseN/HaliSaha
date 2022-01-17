34dabd91a888f8575788cbd7f4087733b5cacef2
iammert <mertcanbenli99@outlook.com>
2022-01-18 00:26:50 +0300

    add location to members

--- HaliSaha/Models/Members.cs
+++ HaliSaha/Models/Members.cs
@@ -2,10 +2,11 @@
 {
     public class Members
     {
-        public string ID { get; set; }
+        public string ID{ get; set; }
         public string name { get; set; }
         public string surname { get; set; }
         public DateTime birthdate { get; set; }
         public string  password { get; set; }
+        public string location { get; set; }
     }
 }
